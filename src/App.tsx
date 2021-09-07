c1a89f7d02bad7de6e433250202f12db86c5bad1
rachel-rebecca <Rarebecc@umich.edu>
2021-09-06 22:15:45 -0400

    cleaned up app.tsx and changed margin for main section socialposts.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
-import PostForm from './components/PostForm';
 import SocialPosts from './components/SocialPosts';
-import PostInList from './components/PostInList';
-import Post from "./components/Post";
-import {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 function App() {
  
- 
- 
-
   return (
     <div className="App">
      
